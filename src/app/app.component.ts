ee1f82bd81f7af0e2478d365c35ddd271af5d675
Abel Rangel <ransilad@gmail.com>
2019-03-23 13:17:34 -0300

    Adding functionality and send data to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IA-frontend-test';
+  public subscription = { name: '', phone: '', email: '', dni: '' };
+
+  constructor(private httpService: HttpService){}
+
+  postSubscriptions() {
+    this.httpService.post_subscriptions(this.subscription).subscribe(apiResult => {
+      if (apiResult.success) {
+        alert('Se grabó exitosamente');
+        this.subscription = { name: '', phone: '', email: '', dni: '' };
+      } else {
+        console.log('Error');
+      }
+    });
+  }
 }
---
+++ src/app/http.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  baseUrl:string = "http://127.0.0.1:5000";
+
+  constructor(private httpClient : HttpClient) {}
+
+  post_subscriptions(jsonData){
+    return this.httpClient.post<any>(this.baseUrl + '/landing/subscriptions', jsonData);
+  }
+}
